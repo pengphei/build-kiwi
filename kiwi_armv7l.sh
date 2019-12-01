535babd09a9edfc3fbe59f2f9a1b4af0c90bc3e3
Han Pengfei (kurain) <pengphei@foxmail.com>
2019-12-01 16:46:53 +0800

    Update kiwi_armv7l.sh

--- kiwi_armv7l.sh
+++ kiwi_armv7l.sh
@@ -1,3 +1,3 @@
 sudo kiwi-ng --type oem system build \
   --description kiwi-descriptions/suse/armv7l/suse-tumbleweed-panda-JeOS \
-  --target-dir /home/kurain/Public/panda
+  --target-dir /opt/panda
